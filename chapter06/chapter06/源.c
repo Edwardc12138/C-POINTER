398c89863aec30bd39d4e08be334a096fb694ecf
Fisio <18702957328@163.com>
2019-04-09 16:04:14 +0800

    指针练习4-6

--- chapter06/chapter06/源.c
+++ chapter06/chapter06/源.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "BitArray.h"
 
 // 查找source中匹配chars中字符的第一个字符并返回指向这个字符的指针
-char *find_char(const char *source, const char *chars) {
+const char *find_char(const char *source, const char *chars) {
 	// 指针为空或字符串为空字符串
 	if (source == NULL || chars == NULL || *source == '\0' || *chars == '\0') {
 		return NULL;
@@ -66,6 +67,7 @@ void reverse_string(char *str) {
 		++tmp;
 		++len;
 	}
+	// 首尾交换
 	for (int left = 0, right = len - 1; left < right;++left, --right) {
 		char ch = *(str + left);
 		*(str + left) = *(str + right);
@@ -74,14 +76,101 @@ void reverse_string(char *str) {
 }
 
 // Eratosthenes筛选法求质数
-void Prame(char *begin, char *end) {
-	for (char *p = begin + 1; p != end; ++p) {
+// 数组表示该位是不是质数,1表示是,0表示不是
+// 偶数只有2是质数,所以数组中只存奇数
+// arr[n]对应数字2*n+3
 
+// 使用字符数组
+void Prame(char *arr, const int size) {
+	if (arr == NULL) {
+		return;
+	}
+	// 将所有元素值设为TRUE
+	for (int i = 0; i < size; ++i) {
+		*(arr + i) = 1;
+	}
+	// 剔除偶数,在对应的元素上置0
+	for (int curr = 0; curr < size; ++curr) {
+		if (*(arr + curr)) {	// 该位置未被剔除
+			int span = 2 * curr + 3;	// 判断的步长
+			for (int del = curr + span; del < size; del += span) {
+				*(arr + del) = 0;			// 剔除该位置
+			}
+		}		
+	}
+}
+
+// 使用位数组
+void PrameUseBit(char *arr, int size) {
+	if (arr == NULL) {
+		return;
+	}
+	size *= 8;					// 函数修改部分
+	// 将所有元素值设为TRUE
+	for (int i = 0; i < size; ++i) {
+		set_bit(arr, i);		// 函数修改部分
+	}
+	// 剔除偶数,在对应的元素上置0
+	for (int curr = 0; curr < size; ++curr) {
+		if (test_bit(arr,curr)) {	// 该位置未被剔除 // 函数修改部分
+			int span = 2 * curr + 3;	// 判断的步长
+			for (int del = curr + span; del < size; del += span) {
+				clear_bit(arr, del);	// 剔除该位置 // 函数修改部分
+			}
+		}		
+	}
+}
+
+// 统计每1000个数有多少个质数并打印
+void CountPrame(char *arr, int size) {
+	if (arr == NULL) {
+		return;
+	}
+	PrameUseBit(arr, size);		// 筛选质数
+	size *= 8;
+	int count = 2;		// 数组中没有2,0~1000加上1
+	int frist = 1;		// 第一次执行循环标志
+	for (int curr = 1; curr < size; ++curr) {
+		if (!frist) {	// 第一次不输出
+			if (!(curr % 1000)) {		// 每隔1000个数输出一次计数
+				printf("%d ", count);
+				count = 0;				// 计数清零
+			}
+		}
+		else {
+			frist = 0;
+		}
+		if (test_bit(arr, curr)) {
+			++count;
+		}
 	}
 }
 
 int main() {
-	// 4.
+	// 6.每隔1000个元素有多少质数
+	char arr[125000];
+	CountPrame(arr, 125000);
+
+	// 5.使用位数组函数
+	//char arr[1000];
+	//PrameUseBit(arr, 1000);
+	//printf("2");
+	//for (int i = 0; i < 8000; ++i) {	// 循环次数需更改
+	//	if (test_bit(arr,i)) {			// 判断条件需更改
+	//		printf(" %d", 2 * i + 3);
+	//	}
+	//}
+
+	// 4.Eratosthenes筛选法
+	//char arr[1000];
+	//Prame(arr, 1000);
+	//printf("2");
+	//for (int i = 0; i < 1000; ++i) {
+	//	if (arr[i] == 1) {
+	//		printf(" %d", 2 * i + 3);
+	//	}
+	//}
+
 
 	// 3.编写函数,逆置字符串
 	//char arr[] = "i have a pen!";
